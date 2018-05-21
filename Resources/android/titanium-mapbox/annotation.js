745987c58e505e82bdc2e6cf49988418a6b13281
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-05-21 19:54:09 +0200

    Update annotation.js

--- Resources/android/titanium-mapbox/annotation.js
+++ Resources/android/titanium-mapbox/annotation.js
@@ -4,11 +4,11 @@ const LatLng = require('com.mapbox.mapboxsdk.geometry.LatLng');
 export default class Annotation {
 	constructor(args) {
 		if (!args.latitude || !args.longitude) {
-			throw 'Missing latitude / longitude';
+			throw new Error('Missing latitude / longitude');
 		}
 
 		this.annotation = new MarkerOptions().setTitle(args.title).setPosition(new LatLng(args.latitude, args.longitude));
-  }
+ 	}
 
 	getInstance() {
 		return this.annotation;
